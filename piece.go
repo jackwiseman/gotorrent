7caf611de32f766ecafcdea948d672759e6556e7
Jack Wiseman <jackwiseman4@gmail.com>
2022-04-17 00:32:13 -0400

    update piece structure

--- piece.go
+++ piece.go
@@ -2,5 +2,8 @@ package main
 
 // Piece stores a collection of blocks, so that a torrent file can be easily written
 type Piece struct {
-	blocks []Block
+	blocks   []Block
+	hash     [20]byte // sha1 hash
+	verified bool     // whether this piece has been verified via sha1 hash
+	numSet   int      // number of blocks that currently have data in them
 }
