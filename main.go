7582d4cc133c3c2b567bd13a40a33ceb4697e9cf
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2024-10-21 18:59:04 -0700

    add zerolog

--- main.go
+++ main.go
@@ -5,23 +5,35 @@ import (
 	"fmt"
 	"gotorrent/models"
 	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/pkgerrors"
 )
 
 // var seed bool
 var connections int
+var debug bool
 
 func init() {
 	// flag.BoolVar(&seed, "seed", false, "continue seeding after download")
 	flag.IntVar(&connections, "connections", 50, "number of connections to use")
+	flag.BoolVar(&debug, "debug", false, "enable debug logging")
 	flag.Parse()
 }
 
 func main() {
 	// flush current debug log
+	// TODO: deprecate old debug logs
 	err := os.Remove("debug.log")
 	if err != nil {
 		fmt.Println("[WARN] no debug.log found")
 	}
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
 
 	if len(os.Args) < 2 {
 		fmt.Printf("Provide a magnet link\n")
